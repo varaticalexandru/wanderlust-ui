a5640aba735b25c28dfb685058ead1e86512c21c
alex <varaticalexandru1@gmail.com>
2024-03-07 00:22:27 +0200

    add search-result-card component; upd destination: changed search results displaying format;

---
+++ src/app/components/preferences/destination/search-result-card/search-result-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Destination } from 'src/app/models/user-destination';
+
+@Component({
+  selector: 'app-search-result-card',
+  templateUrl: './search-result-card.component.html',
+  styleUrls: ['./search-result-card.component.scss']
+})
+export class SearchResultCardComponent {
+
+  @Input() destination!: Destination;
+
+}
