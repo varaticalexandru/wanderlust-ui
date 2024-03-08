eb380a93edd1e8068226c33ba621cd1dc3713b7e
alex <varaticalexandru1@gmail.com>
2024-03-08 22:33:24 +0200

    upd environment & environment-prod template files: add latest decoupled urls/paths

--- src/environments/template_environment-prod.ts
+++ src/environments/template_environment-prod.ts
@@ -1,8 +1,12 @@
 export const environment = {
-    production: true,
+    production: false,
     amadeus: {
         api_key: '',
         api_secret: '',
-        token_url: ''
+        auth_url: '',
+        search_destination_url: ''
+    },
+    localStorage: {
+        country_codes_path: ''
     }
 };
