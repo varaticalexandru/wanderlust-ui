d31cde0e38c2f1fee4da3da4600433104bb5662f
alex <varaticalexandru1@gmail.com>
2024-03-10 11:03:38 +0200

    upd environment files: - add new urls for popular-destinations and airport-search;

--- src/environments/template_environment-prod.ts
+++ src/environments/template_environment-prod.ts
@@ -4,7 +4,9 @@ export const environment = {
         api_key: '',
         api_secret: '',
         auth_url: '',
-        search_destination_url: ''
+        search_destination_url: '',
+        popular_destinations_url: '',
+        airport_search_url: ''
     },
     localStorage: {
         country_codes_path: ''
