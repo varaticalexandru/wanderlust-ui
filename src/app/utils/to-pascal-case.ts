0f2b8ec6929fe1452138a4a1f520db91fb9a6399
alex <varaticalexandru1@gmail.com>
2024-03-11 11:17:07 +0200

    add utils function: - converts string to pascal case;

---
+++ src/app/utils/to-pascal-case.ts
@@ -0,0 +1,9 @@
+export function toPascalCase (str: string): string {
+    if (/^[\p{L}\d]+$/iu.test(str)) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    return str.replace(
+      /([\p{L}\d])([\p{L}\d]*)/giu,
+      (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase()
+    ).replace(/[^\p{L}\d]/giu, '');
+  }
