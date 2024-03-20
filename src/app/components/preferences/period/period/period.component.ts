6fe17f6c70eefae08eea639c11610d26c95bc110
alex <varaticalexandru1@gmail.com>
2024-03-20 20:11:14 +0200

    component init: add routing from destination

---
+++ src/app/components/preferences/period/period/period.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-period',
+  templateUrl: './period.component.html',
+  styleUrls: ['./period.component.scss']
+})
+export class PeriodComponent {
+
+}
