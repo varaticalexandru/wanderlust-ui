d8971fbe5f03d18d4cf27391ee3a3650b5f19e8d
alex <varaticalexandru1@gmail.com>
2024-03-21 00:44:59 +0200

    add period component

---
+++ src/app/components/preferences/period/period.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PeriodComponent } from './period.component';
+
+describe('PeriodComponent', () => {
+  let component: PeriodComponent;
+  let fixture: ComponentFixture<PeriodComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PeriodComponent]
+    });
+    fixture = TestBed.createComponent(PeriodComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
