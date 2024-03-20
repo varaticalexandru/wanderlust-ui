16e267b8b22ee24ee847ebfc890e2d07c2fc672a
alex <varaticalexandru1@gmail.com>
2024-03-20 16:11:29 +0200

    upd to-title-case.ts: - add toSentenceTitleCase() method;

--- src/app/utils/to-title-case.ts
+++ src/app/utils/to-title-case.ts
@@ -1,3 +1,7 @@
 export function toTitleCase (str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export function toSentenceTitleCase (str: string): string {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
