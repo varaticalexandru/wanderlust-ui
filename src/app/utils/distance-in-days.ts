e7a466d2f668856383ba8dbaf33f99ed6a8d197a
alex <varaticalexandru1@gmail.com>
2024-04-06 12:12:05 +0300

    add helper function to calculate distance between 2 dates in days

---
+++ src/app/utils/distance-in-days.ts
@@ -0,0 +1,5 @@
+export function differenceInDays (date1: Date, date2: Date): number {
+    const dayInMs = 24 * 60 * 60 * 1000;
+    const differenceInMilliseconds = Math.abs(date1.getTime() - date2.getTime());
+    return Math.round(differenceInMilliseconds / dayInMs);
+  }
