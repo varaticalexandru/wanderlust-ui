0c8fbf21821f68b3d58ffa90174b405cd35a6031
alex <varaticalexandru1@gmail.com>
2024-04-06 12:11:17 +0300

    upd period component: add date-range-picker

--- src/app/components/preferences/period/period.component.ts
+++ src/app/components/preferences/period/period.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DateRange } from 'igniteui-angular';
+import { differenceInDays } from 'src/app/utils/distance-in-days';
 
 @Component({
   selector: 'app-period',
@@ -9,19 +10,27 @@ import { DateRange } from 'igniteui-angular';
 })
 export class PeriodComponent implements OnInit {
 
-  range: DateRange = { 
-    start: new Date(2024, 3, 20), 
-    end: new Date(2024, 3, 25) 
-  };
+  range!: DateRange;
+  minDate: Date;
+  maxDate: Date;
+  isRangeValid!: boolean;
 
-  constructor() {
-    router: Router
+  constructor(
+    private router: Router
+  ) {
+
+    this.isRangeValid = true;
+    this.minDate = new Date();
+    this.maxDate = new Date(this.minDate.getFullYear() + 1, 12 - 1, 31);
   }
 
   ngOnInit(): void {
-   
+
   }
 
-  
-
+  onRangeChange(newRange: DateRange) {
+    this.isRangeValid = differenceInDays(new Date(newRange.start), new Date(newRange.end)) >= 6
+      ? false
+      : true;
+  }
 }
