3d14f784dff040320bf256bfbb8c5f39a5453f83
alex <varaticalexandru1@gmail.com>
2024-04-15 18:12:43 +0300

    companion component init

---
+++ src/app/components/preferences/companion/companion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-companion',
+  standalone: true,
+  imports: [],
+  templateUrl: './companion.component.html',
+  styleUrl: './companion.component.scss'
+})
+export class CompanionComponent {
+
+}
