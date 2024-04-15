93940c7b69c23388f5f56479e03e0e12b9119c9e
alex <varaticalexandru1@gmail.com>
2024-04-15 14:14:39 +0300

    upd distance-in-days: - refactor to compute the number of days (inclusive) in a range of 2 dates, instead of the difference;

--- src/app/utils/distance-in-days.ts
+++ src/app/utils/distance-in-days.ts
@@ -1,5 +1,5 @@
-export function differenceInDays (date1: Date, date2: Date): number {
+export function daysNumberInRange (date1: Date, date2: Date): number {
     const dayInMs = 24 * 60 * 60 * 1000;
     const differenceInMilliseconds = Math.abs(date1.getTime() - date2.getTime());
-    return Math.round(differenceInMilliseconds / dayInMs);
+    return Math.round(differenceInMilliseconds / dayInMs) + 1;
   }
