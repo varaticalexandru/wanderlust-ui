aed08fe6f341cfa7f1a9b4855c861baef8ab2aa7
alex <varaticalexandru1@gmail.com>
2024-04-23 17:27:31 +0300

    add data: companions

---
+++ src/app/data/companion.data.ts
@@ -0,0 +1,20 @@
+import { Companion } from "..\\models\\companion.model";
+
+export let companions: Array<Companion> = [
+    {
+        title: 'Solo',
+        image: "assets\\media\\person_FILL0_wght400_GRAD0_opsz24.svg"
+    },
+    {
+        title: 'Partner',
+        image: "assets\\media\\diversity_1_FILL0_wght400_GRAD0_opsz24.svg"
+    },
+    {
+        title: 'Friends',
+        image: "assets\\media\\diversity_3_FILL0_wght400_GRAD0_opsz24.svg"
+    },
+    {
+        title: 'Family',
+        image: "assets\\media\\family_restroom_FILL0_wght400_GRAD0_opsz24.svg"
+    }
+];
