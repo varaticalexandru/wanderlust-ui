c13155e72da6b2d03642850b0c7abe2f176c8fb9
alex <varaticalexandru1@gmail.com>
2024-04-29 15:50:16 +0300

    add chips component

--- src/app/components/preferences/interests/chips/chips.component.ts
+++ src/app/components/preferences/interests/chips/chips.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
@@ -15,7 +15,30 @@ export class ChipsComponent {
   @Input()
   options!: Array<string>;
 
-  constructor() {
+  selectedOptions!: Array<string>;
 
+  @Output()
+  selectionChange = new EventEmitter<Array<string>>;
+
+  constructor() {
+    this.selectedOptions = [];
+  }
+
+  onSelectionChange(index: number): void {
+
+    let arrayOption = this.options.at(index) as string;
+    let isChecked = this.selectedOptions.find((option: string) => option === arrayOption);
+    if (!isChecked)  
+      this.selectedOptions.push(arrayOption);
+    else {
+      let slice_idx = this.selectedOptions.indexOf(arrayOption);
+      this.selectedOptions.splice(slice_idx, 1);
+    }
+
+    this.selectionChange.emit(this.selectedOptions);
+  }
+
+  getSelectedOptions(): Array<string> {
+    return this.selectedOptions;
   }
 }
