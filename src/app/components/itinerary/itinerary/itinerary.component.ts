716a5e5f73717aa9f89e5476c0a4c6e3080c75be
alex <varaticalexandru1@gmail.com>
2024-05-20 10:48:47 +0300

    add itinerary component with travel plan and recommendations

---
+++ src/app/components/itinerary/itinerary/itinerary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itinerary',
+  standalone: true,
+  imports: [],
+  templateUrl: './itinerary.component.html',
+  styleUrl: './itinerary.component.scss'
+})
+export class ItineraryComponent {
+
+}
