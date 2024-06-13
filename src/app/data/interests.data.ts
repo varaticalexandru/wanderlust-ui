774f9d35c39abad698e4ddd77f84c156c14050f4
alex <varaticalexandru1@gmail.com>
2024-06-13 13:52:04 +0300

    refactor interests data: add categories & icons for each category and item

--- src/app/data/interests.data.ts
+++ src/app/data/interests.data.ts
@@ -1,69 +1,119 @@
-export let interests: Array<string> = [
-    // Food and Drink
-    "Great Food",
-    "Local Cuisine",
-    "Vineyards and Breweries",
-    "Pubs and Bars",
-    "Street Food Markets",
-    "Food Tours",
-  
-    // Cultural and Historical
-    "Art Galleries",
-    "Museums",
-    "History",
-    "Culture",
-    "Historical Reenactments",
-    "Literature",
-    "Spiritual Sites",
-    "Archaeological Sites",
-    "Cultural Workshops",
-    "Cinema",
-  
-    // Nature and Outdoors
-    "Nature Reserves",
-    "Outdoors",
-    "Beaches",
-    "Scenic Drives",
-    "Eco-tourism",
-    "Hiking Trails",
-    "Wildlife Safaris",
-    "National Parks",
-    "Gardens and Parks",
-  
-    // Entertainment and Events
-    "Must-see Attractions",
-    "Iconic Landmarks",
-    "Theatre and Performing Acts",
-    "Music Festivals",
-    "Nightlife",
-    "Entertainment",
-    "Clubs",
-    "Seasonal Events",
-    "Live Music Venues",
-    "Carnivals",
-    "Street Performances",
-  
-    // Shopping and Crafts
-    "Shopping",
-    "Local Crafts",
-    "Artisan Markets",
-    "Souvenir Shops",
-    "Fashion Districts",
-  
-    // Wellness and Leisure
-    "Wellness & Spas",
-    "Family Attractions",
-    "Adventure and Sports",
-    "Yoga Retreats",
-    "Fitness Centers",
-    "Amusement Parks",
-    "Water Parks",
-  
-    // Miscellaneous
-    "Hidden Gems",
-    "Photography Hotspots",
-    "Volunteer Opportunities",
-    "Astronomy Stargazing",
-    "Geocaching",
-    "Historic Railways"
-  ];
+import { CategoryInterests } from '../models/interests.model';
+
+export const interests: Array<CategoryInterests> = [
+  {
+    category: 'Food and Drinks 🍔',
+    items: [
+      '🍽️ Great Food',
+      '🍲 Local Cuisine',
+      '🍷 Vineyards and Breweries',
+      '🍻 Pubs and Bars',
+      '🍜 Street Food Markets',
+      '🍱 Food Tours',
+    ],
+  },
+
+  {
+    category: 'Cultural and Historical 🕌',
+    items: [
+      '🖼️ Art Galleries',
+      '🏛️ Museums',
+      '📜 History',
+      '🎭 Culture',
+      '⚔️ Historical Reenactments',
+      '📚 Literature',
+      '⛪ Spiritual Sites',
+      '🏺 Archaeological Sites',
+      '🎬 Cinema',
+      '🎐 Local Traditions',
+    ],
+  },
+
+  {
+    category: 'Nature and Outdoors 🌄',
+    items: [
+      '🌲 Nature Reserves',
+      '🌳 Outdoors',
+      '🏖️ Beaches',
+      '🚗 Scenic Drives',
+      '♻️ Eco-tourism',
+      '🥾 Hiking Trails',
+      '🦁 Wildlife Safaris',
+      '🏞️ National Parks',
+      '🌸 Gardens and Parks',
+      '🧗‍♂️ Mountain Climbing',
+      '🦜 Bird Watching',
+      '🏕️ Camping',
+      '🌊 Waterfalls',
+    ],
+  },
+
+  {
+    category: 'Entertainment and Events 🍿',
+    items: [
+      '🌟 Must-see Attractions',
+      '🗽 Iconic Landmarks',
+      '🎭 Theatre and Performing Acts',
+      '🎵 Music',
+      '🌃 Nightlife',
+      '🎡 Entertainment',
+      '🎉 Clubs',
+      '🎊 Seasonal Events',
+      '⚽ Sporting Events',
+      '🎤 Live Music Venues',
+      '🎡 Carnivals',
+      '🎪 Street Performances',
+    ],
+  },
+
+  {
+    category: 'Shopping and Crafts 🏬',
+    items: [
+      '🛍️ Shopping',
+      '🧶 Local Crafts',
+      '🏺 Artisan Markets',
+      '🎁 Souvenir Shops',
+      '👗 Fashion Districts',
+      '🕰️ Antique Shops',
+      '💎 Luxury Shopping',
+      '🛒 Flea Markets',
+    ],
+  },
+
+  {
+    category: 'Wellness and Leisure 🧑‍⚕️',
+    items: [
+      '💆‍♀️ Wellness & Spas',
+      '👨‍👩‍👧‍👦 Family Attractions',
+      '🏄‍♂️ Adventure and Sports',
+      '🧘‍♀️ Yoga Retreats',
+      '🏋️‍♂️ Fitness Centers',
+      '🎢 Amusement Parks',
+      '🏊‍♂️ Water Parks',
+      '♨️ Hot Springs',
+      '🧘‍♂️ Meditation',
+      '🏌️‍♂️ Golf',
+      '🚴‍♂️ Cycling',
+    ],
+  },
+
+  {
+    category: 'Miscellaneous ⭕',
+    items: [
+      '💎 Hidden Gems',
+      '📸 Photography Hotspots',
+      '🌌 Stargazing',
+      '🔭 Astronomy',
+      '🚂 Historic Railways',
+      '🛣️ Road Trips',
+      '🛥️ Boat Tours',
+      '🚶‍♂️ City Walks',
+      '🤝 Volunteering',
+      '🗣️ Language Learning',
+      '👩‍🍳 Cooking Classes',
+      '🎎 Local Festivals',
+      '🏇 Horseback Riding',
+      '🛶 Kayaking and Canoeing',
+    ],
+  },
+];
