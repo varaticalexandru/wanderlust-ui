b3adf10849a97a1c756e4ce63cd034404ee1c6db
alex <varaticalexandru1@gmail.com>
2024-06-13 13:38:57 +0300

    upd main.ts: add google maps api init script programatically

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, NgbModule, AppRoutingModule, FormsModule, ReactiveFormsModule),
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
+(window as any).googleMapsApiKey = environment.googleMaps.api_key;
+
+function loadGoogleMapsScript(apiKey: string) {
+  const script = document.createElement('script');
+  script.innerHTML = `
+    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=\`https://maps.\${c}apis.com/maps/api/js?\`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+      v: "beta",
+      key: "${apiKey}"
+    });
+  `;
+  document.body.appendChild(script);
+}
+
+loadGoogleMapsScript((window as any).googleMapsApiKey);
+
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
