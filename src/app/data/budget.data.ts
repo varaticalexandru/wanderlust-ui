e9fbd673f8ef0a143f8c9c13ecb5d78e4dd85d3b
alex <varaticalexandru1@gmail.com>
2024-06-13 14:31:41 +0300

    finish priceLevel to budget model renaming

--- src/app/data/budget.data.ts
+++ src/app/data/budget.data.ts
@@ -1,6 +1,6 @@
-import { Budget } from '../models/price-level.model';
+import { Budget } from '../models/budget.model';
 
-export const priceLevels: Array<Budget> = [
+export const budgets: Array<Budget> = [
   {
     value: 1,
     name: 'Low',
