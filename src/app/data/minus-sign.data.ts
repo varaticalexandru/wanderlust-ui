28fbcb05f3702c6a15525805c96f0e65d0cb6512
alex <varaticalexandru1@gmail.com>
2024-06-13 13:54:15 +0300

    add minus sign/bar svg string for color displaying/matching with a corresponding  travel day destinations/locations/recommendations on the daily-card and map

---
+++ src/app/data/minus-sign.data.ts
@@ -0,0 +1,6 @@
+export const minus_sign_svg_string: string = `<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#COLOR_PLACEHOLDER" version="1.1" id="Capa_1" width="60px" height="30px" viewBox="0 0 35.618 35.619" xml:space="preserve">
+<g>
+	<path d="M35.618,14.826v5.965c0,2.027-1.642,3.668-3.666,3.668H3.666C1.643,24.458,0,22.818,0,20.791v-5.965   c0-2.022,1.643-3.666,3.666-3.666h28.285C33.977,11.16,35.618,12.802,35.618,14.826z"></path>
+</g>
+</svg>`;
