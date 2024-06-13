87f380d4f848f426772a3e566053088b6e965990
alex <varaticalexandru1@gmail.com>
2024-06-13 13:41:29 +0300

    upd itinerary api: export/expose module routes for lazy-loading

---
+++ src/app/components/itinerary/api/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ItineraryComponent } from "../itinerary.component";
+
+export const ITINERARY_ROUTES: Routes = [
+    {
+        path: "",
+        component: ItineraryComponent 
+    }
+]
