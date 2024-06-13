c6684bae958a97a8fb606b4ee07dc5072bbda7ed
alex <varaticalexandru1@gmail.com>
2024-06-13 13:50:24 +0300

    add user's trip/travel relative budget availability data file with 4 options: Low, Medium, High, Luxury

---
+++ src/app/data/budget.data.ts
@@ -0,0 +1,20 @@
+import { PriceLevel } from '../models/price-level.model';
+
+export const priceLevels: Array<PriceLevel> = [
+  {
+    value: 1,
+    name: 'Low',
+  },
+  {
+    value: 2,
+    name: 'Medium',
+  },
+  {
+    value: 3,
+    name: 'High',
+  },
+  {
+    value: 4,
+    name: 'Luxury',
+  }
+];
