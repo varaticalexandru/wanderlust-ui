6d198a8a570c60134e9152aa9713fc395720a677
alex <varaticalexandru1@gmail.com>
2024-06-16 17:11:57 +0300

    upd text-search service: add fetchPlacePhotoIds, fetchPlaceId methods

--- src/app/services/google-maps/text-search/text-search.service.ts
+++ src/app/services/google-maps/text-search/text-search.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PlacePhotosResponse } from 'src/app/models/google-maps/place-photo.model';
 import {
   PlacesDetailsRequest,
   Place,
@@ -17,7 +18,11 @@ export class TextSearchService {
     'places.id',
     'places.location',
     'places.displayName.text',
+    'places.photos.name',
   ];
+
+  id_fields: string = 'places.id';
+  photos_fields: string = 'photos.name';
 
   options = {
     headers: new HttpHeaders()
@@ -26,7 +31,10 @@ export class TextSearchService {
       .append('X-Goog-Api-Key', `${environment.googleMaps.api_key}`),
   };
 
-  text_search_url = `${environment.googleMaps.search_text_url}?fields=${this.fields.join()}`
+
+  text_search_url = `${environment.googleMaps.search_text_uri}?fields=${this.fields.join()}`
+  place_id_url = `${environment.googleMaps.search_text_uri}?fields=${this.id_fields}`
+  place_photos_url = `${environment.googleMaps.place_details_uri}/places`
 
   constructor(private http: HttpClient) {}
 
@@ -36,6 +44,23 @@ export class TextSearchService {
     return this.http.post<PlaceDetailsResponse>(
       this.text_search_url,
       placeDatailsReq,
+      this.options
+    );
+  }
+
+  fetchPlaceId(
+    placeDatailsReq: PlacesDetailsRequest
+  ): Observable<PlaceDetailsResponse> {
+    return this.http.post<PlaceDetailsResponse>(
+      this.place_id_url,
+      placeDatailsReq,
+      this.options
+    );
+  }
+
+  fetchPlacePhotoIds(placeId: string): Observable<PlacePhotosResponse> {
+    return this.http.get<PlacePhotosResponse>(
+      `${this.place_photos_url}/${placeId}?fields=${this.photos_fields}`,
       this.options
     );
   }
