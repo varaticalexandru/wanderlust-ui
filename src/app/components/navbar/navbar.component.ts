9b6dbf1e83c9dc81090a476402f408c863e182ee
alex <varaticalexandru1@gmail.com>
2024-06-18 09:50:40 +0300

    upd navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,7 +4,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { LogOutComponent } from '../login/log-out/log-out.component';
+import { LogOutConfirmComponent } from '../login/log-out-confirm/log-out-confirm.component';
+import { AuthService } from 'src/app/services/auth/auth-service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -22,8 +26,19 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
   constructor(
+    private dialog: MatDialog,
+    private router: Router,
+    private authService: AuthService
   ) {}
 
+  logout() {
+    const dialogRef = this.dialog.open(LogOutConfirmComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.authService.logout();
+      }
+    });
+  }
 }
