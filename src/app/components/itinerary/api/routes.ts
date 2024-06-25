7fa2f5df0a7cf40759d3f1129e995cdd13046203
alex <varaticalexandru1@gmail.com>
2024-06-25 13:17:25 +0300

    upd itinerary routes: add showNavbar flag and authentication guard

--- src/app/components/itinerary/api/routes.ts
+++ src/app/components/itinerary/api/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from "@angular/router";
 import { ItineraryComponent } from "../itinerary.component";
+import { authGuard } from "src/app/guards/auth/auth.guard";
 
 export const ITINERARY_ROUTES: Routes = [
     {
         path: "",
-        component: ItineraryComponent 
+        component: ItineraryComponent,
+        data: { showNavbar: true },
+        canActivate: [authGuard],
     },
 ]
