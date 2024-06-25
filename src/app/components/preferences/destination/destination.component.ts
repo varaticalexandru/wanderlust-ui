4414df7d68118ba78d5ba3b9aa36d40fc990d405
alex <varaticalexandru1@gmail.com>
2024-06-25 20:27:36 +0300

    upd destination component: - refactor code - add footer

--- src/app/components/preferences/destination/destination.component.ts
+++ src/app/components/preferences/destination/destination.component.ts
@@ -3,7 +3,17 @@ import { Router, RouterOutlet } from '@angular/router';
 import { PopularDestination } from 'src/app/models/popular-destination.model';
 import { Destination } from 'src/app/models/user-destination.model';
 import { SearchDestinationService } from 'src/app/services/search/search-destination.service';
-import { Observable, Subject, forkJoin, map, mergeMap, of, switchMap, tap } from 'rxjs';
+import {
+  Observable,
+  Subject,
+  catchError,
+  forkJoin,
+  map,
+  mergeMap,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { liveSearch } from 'src/app/utils/operators/live-search';
 import { CountryMappingService } from 'src/app/services/country-mapping/country-mapping.service';
 import { PopularDestinationsService } from 'src/app/services/popular-destinations/popular-destinations.service';
@@ -15,7 +25,7 @@ import { Medias } from 'src/app/models/pixabay/medias.model';
 import { MediaService } from 'src/app/services/media/media.service';
 import { toSentenceTitleCase, toTitleCase } from 'src/app/utils/to-title-case';
 import { randomInt } from 'src/app/utils/random-int';
-import { getRandomElements } from 'src/app/utils/random-arr-elements'
+import { getRandomElements } from 'src/app/utils/random-arr-elements';
 import { AmadeusDestinations } from 'src/app/models/amadeus/amadeus-destinations.model';
 import { MatButton } from '@angular/material/button';
 import { MatProgressBar } from '@angular/material/progress-bar';
@@ -25,9 +35,20 @@ import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatInput } from '@angular/material/input';
-import { MatFormField, MatLabel, MatPrefix } from '@angular/material/form-field';
-import { MatCard, MatCardTitle, MatCardContent, MatCardFooter } from '@angular/material/card';
+import {
+  MatFormField,
+  MatLabel,
+  MatPrefix,
+} from '@angular/material/form-field';
+import {
+  MatCard,
+  MatCardTitle,
+  MatCardContent,
+  MatCardFooter,
+} from '@angular/material/card';
 import { PreferencesService } from 'src/app/services/preferences/preferences.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-destination',
@@ -52,35 +73,38 @@ import { PreferencesService } from 'src/app/services/preferences/preferences.ser
     MatProgressBar,
     MatButton,
     RouterOutlet,
-    AsyncPipe
-  ]
+    AsyncPipe,
+    FooterComponent,
+  ],
 })
 export class DestinationComponent implements OnInit {
-
   private searchTerm = new Subject<string>();
   searchTermString: string = '';
   destinations: Array<Destination> = [];
   popularDestinations$!: Observable<PopularDestination[]>;
   selectedDestination!: Destination;
   popularDestinationsNumber: number = 3;
-  isLoading: boolean;
+  isLoading: boolean = true;
 
   readonly destinations$ = this.searchTerm.pipe(
-    liveSearch((term: string) => this.destinationService.searchDestinations(term)),
-    switchMap((destinations: AmadeusDestinations): Observable<Destination[]> => {
-
-      return destinations.data ?
-        of(destinations.data.map(destination => {
-          return {
-            cityName: destination.name,
-            countryName: this.getCountryName(destination.address.countryCode)
-          };
-
-        })) : of([]);
-
+    liveSearch((term: string) =>
+      this.destinationService.searchDestinations(term)
+    ),
+    switchMap(
+      (destinations: AmadeusDestinations): Observable<Destination[]> =>
+        this.mapDestinations(destinations)
+    ),
+    catchError((error: any) => {
+      console.error('Error fetching destinations: ', error);
+      this.snackBar.open('Error fetching destinations ❌', 'Retry', {
+        duration: 5000,
+        politeness: 'assertive',
+      }).onAction().subscribe(() => {
+        this.search(this.searchTermString);
+      });
+      return of([]);
     })
   );
-
 
   constructor(
     private router: Router,
@@ -90,52 +114,104 @@ export class DestinationComponent implements OnInit {
     private popularDestinationsService: PopularDestinationsService,
     private airportCityMappingService: AirportCityMappingService,
     private mediaService: MediaService,
-    private preferencesService: PreferencesService
-  ) {
-
-    this.isLoading = true;
-  }
+    private preferencesService: PreferencesService,
+    private snackBar: MatSnackBar,
+  ) {}
 
   ngOnInit(): void {
-
-    this.popularDestinations$ = this.popularDestinationsService.popularDestinations$.pipe(
-
-      mergeMap((popularDestinations: PopularDestinations): Observable<Airports[]> => {
-        const randomPopularDestinations = getRandomElements(popularDestinations.data, this.popularDestinationsNumber);
-        const destinationNames$: Observable<Airports>[] = randomPopularDestinations.map(
-          (destination: any) =>
-            this.airportCityMappingService.getAirportDetailsByCode(destination.destination)
-        );
-
-        return forkJoin(destinationNames$);
-      }),
-      mergeMap((destinations: Airports[]): Observable<PopularDestination[]> => {
-
-        const destinationsImages$: Observable<PopularDestination>[] = destinations.map((destination: Airports) => {
-          return this.mediaService.fetchMediaByQuery(destination.data[0].address.cityName).pipe(
-            map((medias: Medias): PopularDestination => {
-              const randomIndex = randomInt(medias.hits.length);
-              return {
-                cityName: toSentenceTitleCase(destination.data[0].address.cityName),
-                countryName: toSentenceTitleCase(destination.data[0].address.countryName),
-                image: medias.hits[randomIndex].webformatURL
-              }
-            })
-          )
-        });
-
-        return forkJoin(destinationsImages$);
-      }),
-      tap((popularDestinations: PopularDestination[]) => this.isLoading = false)
-    );
-
+    this.popularDestinations$ = this.getPopularDestinations();
   }
 
+  private fetchPopularDestinations(): void {
+    this.isLoading = true;
+    this.popularDestinations$ = this.getPopularDestinations().pipe(
+      tap(() => (this.isLoading = false))
+    );
+  }
+
+  private getPopularDestinations(): Observable<PopularDestination[]> {
+    return this.popularDestinationsService.popularDestinations$.pipe(
+      switchMap((popularDestinations: PopularDestinations) =>
+        this.fetchAirportDetails(popularDestinations)
+      ),
+      switchMap((destinations: Airports[]) =>
+        this.fetchDestinationImages(destinations)
+      ),
+      catchError((error: any) => {
+        console.error('Error fetching popular destinations: ', error);
+        this.snackBar.open('Error fetching popular destinations ❌', 'Retry', {
+          duration: 5000,
+          politeness: 'assertive',
+        }).onAction().subscribe(() => {
+          this.fetchPopularDestinations();
+        });
+        return of([]);
+      }),
+      tap(() => (this.isLoading = false))
+    );
+  }
+
+  private fetchAirportDetails(
+    popularDestinations: PopularDestinations
+  ): Observable<Airports[]> {
+    const randomPopularDestinations = getRandomElements(
+      popularDestinations.data,
+      this.popularDestinationsNumber
+    );
+    const destinationNames$ = randomPopularDestinations.map(
+      (destination: any) =>
+        this.airportCityMappingService.getAirportDetailsByCode(
+          destination.destination
+        )
+    );
+    return forkJoin(destinationNames$);
+  }
+
+  private fetchDestinationImages(
+    destinations: Airports[]
+  ): Observable<PopularDestination[]> {
+    const destinationsImages$ = destinations.map((destination: Airports) =>
+      this.mediaService
+        .fetchMediaByQuery(destination.data[0].address.cityName)
+        .pipe(
+          map((medias: Medias) =>
+            this.createPopularDestination(destination, medias)
+          )
+        )
+    );
+    return forkJoin(destinationsImages$);
+  }
+
+  private createPopularDestination(
+    destination: Airports,
+    medias: Medias
+  ): PopularDestination {
+    const randomIndex = randomInt(medias.hits.length);
+    return {
+      cityName: toSentenceTitleCase(destination.data[0].address.cityName),
+      countryName: toSentenceTitleCase(destination.data[0].address.countryName),
+      image: medias.hits[randomIndex].webformatURL,
+    };
+  }
+
+  private mapDestinations(
+    destinations: AmadeusDestinations
+  ): Observable<Destination[]> {
+    return destinations.data
+      ? of(
+          destinations.data.map((destination) => ({
+            cityName: destination.name,
+            countryName: this.getCountryName(destination.address.countryCode),
+          }))
+        )
+      : of([]);
+  }
 
   search(searchTerm: string) {
-    let normalizedTerm = searchTerm.trim().toLowerCase();
-
-    normalizedTerm !== '' && normalizedTerm.length >= 3 ? this.searchTerm.next(normalizedTerm) : null;
+    const normalizedTerm = searchTerm.trim().toLowerCase();
+    if (normalizedTerm !== '' && normalizedTerm.length >= 3) {
+      this.searchTerm.next(normalizedTerm);
+    }
   }
 
   getCountryName(code: string): string {
@@ -145,21 +221,26 @@ export class DestinationComponent implements OnInit {
   handleDestinationSelect(destination: any) {
     this.selectedDestination = {
       cityName: destination.cityName,
-      countryName: destination.countryName
+      countryName: destination.countryName,
     };
   }
 
   isEqual(dest1: any, dest2: any): boolean {
-    return dest1.countryName == dest2.countryName &&
-      dest1.cityName == dest2.cityName;
+    return (
+      dest1.countryName === dest2.countryName &&
+      dest1.cityName === dest2.cityName
+    );
   }
 
   back() {
-
+    // Implement back navigation logic if needed
   }
 
   next() {
-    this.preferencesService.setPreference('destination', this.selectedDestination);
+    this.preferencesService.setPreference(
+      'destination',
+      this.selectedDestination
+    );
     this.router.navigate(['/preferences/period']);
   }
 }
