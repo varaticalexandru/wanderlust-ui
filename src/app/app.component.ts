6d163c895f8aca7f12abb0dbc72088bc5337e153
alex <varaticalexandru1@gmail.com>
2024-06-25 13:07:00 +0300

    upd app component: add showNavbar boolean flag and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-// import { AmadeusAuthService } from './services/amadeus-auth/amadeus-auth.service';
-// import { Subscription } from 'rxjs';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [
-      RouterModule,
-      RouterOutlet,
-      NavbarComponent,
-      FooterComponent,
-    ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterModule, RouterOutlet, NavbarComponent, FooterComponent],
 })
 export class AppComponent implements OnInit {
+  showNavbar: boolean = true;
 
-  constructor(
-    // private amadeusAuthService: AmadeusAuthService
-  ) {}
-
-  ngOnInit() {
-    // this.amadeusAuthService.token_data$.subscribe({});
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary')
+      )
+      .subscribe((route) => {
+        this.showNavbar = route.snapshot.data['showNavbar'] !== false;
+      });
   }
 
-  ngOnDestroy() {
-    
-  }
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 }
