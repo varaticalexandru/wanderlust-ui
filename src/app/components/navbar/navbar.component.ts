cccf5da880f16006d8c808644e6b30a006320e82
alex <varaticalexandru1@gmail.com>
2024-06-25 20:25:01 +0300

    upd navbar: - add my-profile section

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { LogOutComponent } from '../login/log-out/log-out.component';
 import { LogOutConfirmComponent } from '../login/log-out-confirm/log-out-confirm.component';
 import { AuthService } from 'src/app/services/auth/auth-service/auth.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
@@ -25,12 +26,21 @@ import { AuthService } from 'src/app/services/auth/auth-service/auth.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  isMobile: boolean = true;
+
   constructor(
     private dialog: MatDialog,
     private router: Router,
     private authService: AuthService
   ) {}
+
+  ngOnInit() {}
+
+  myProfile() {
+    this.router.navigate(['/profile']);
+  }
 
   logout() {
     const dialogRef = this.dialog.open(LogOutConfirmComponent);
