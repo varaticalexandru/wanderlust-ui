829cd5c58c0fd8ab355e9b353c8e48954eb1c152
alex <varaticalexandru1@gmail.com>
2024-06-25 13:30:17 +0300

    upd itinierary-list api (routes): - /itineraries - /itineraries/{id}

--- src/app/components/itinerary-list/api/routes.ts
+++ src/app/components/itinerary-list/api/routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { ItineraryListComponent } from '../itinerary-list.component';
 import { ItineraryViewComponent } from '../itinerary-view/itinerary-view.component';
+import { authGuard } from 'src/app/guards/auth/auth.guard';
 
 export const ITINERARY_LIST_ROUTES: Routes = [
-  { path: '', component: ItineraryListComponent },
-  { path: ':id', component: ItineraryViewComponent },
+  { path: '', component: ItineraryListComponent, data: { showNavbar: true }, canActivate: [authGuard] },
+  { path: ':id', component: ItineraryViewComponent, data: { showNavbar: true }, canActivate: [authGuard] },
 ];
