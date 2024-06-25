2e95186de01212be20069696091fc6b581078649
alex <varaticalexandru1@gmail.com>
2024-06-25 20:25:40 +0300

    upd preferences API (routes): - add auth guard - add showNavbar flag

--- src/app/components/preferences/api/routes.ts
+++ src/app/components/preferences/api/routes.ts
@@ -3,15 +3,16 @@ import { CompanionComponent } from "../companion/companion.component";
 import { DestinationComponent } from "../destination/destination.component";
 import { InterestsComponent } from "../interests/interests.component";
 import { PeriodComponent } from "../period/period.component";
+import { authGuard } from "src/app/guards/auth/auth.guard";
 
 export const PREFERENCES_ROUTES: Routes = [
 
     { path: '', redirectTo: 'destination', pathMatch: 'full' },
   
-    { path: 'destination', component: DestinationComponent },
-    { path: 'period', component: PeriodComponent },
-    { path: 'companion', component: CompanionComponent },
-    { path: 'interests', component: InterestsComponent },
+    { path: 'destination', component: DestinationComponent, data: { showNavbar: true }, canActivate: [authGuard] },
+    { path: 'period', component: PeriodComponent, data: { showNavbar: true }, canActivate: [authGuard] },
+    { path: 'companion', component: CompanionComponent, data: { showNavbar: true }, canActivate: [authGuard] },
+    { path: 'interests', component: InterestsComponent, data: { showNavbar: true }, canActivate: [authGuard] },
   
     { path: '**', redirectTo: 'destination' }
   ];
